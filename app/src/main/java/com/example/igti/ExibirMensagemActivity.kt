1273fa728f6b6d16929d5bf4fc71685bcd7fe52d
Nathan <nathansfaria@hotmail.com>
2022-08-07 21:40:49 -0300

    Criação de segunda tela para exibir mensagem

---
+++ app/src/main/java/com/example/igti/ExibirMensagemActivity.kt
@@ -0,0 +1,18 @@
+package com.example.igti
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class ExibirMensagemActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_exibir_mensagem)
+
+        val mensagem = intent.getStringExtra(EXTRA_MESSAGE)
+
+        val textView = findViewById<TextView>(R.id.textViewMensagem).apply {
+            text = mensagem
+        }
+    }
+}
